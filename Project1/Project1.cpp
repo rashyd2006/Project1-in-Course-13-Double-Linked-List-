b84b99788c150ff6a5f49202d214e34dbc3f47d0
Rashyd <rashydramdwn@gmail.com>
2026-06-08 18:11:15 +0300

    Delete Some line of Code from main.cpp and using the Size function

--- Project1/Project1.cpp
+++ Project1/Project1.cpp
@@ -18,33 +18,7 @@ int main()
     cout << "\n" << "Linked List Content: " << "\n";
     MyDblLinkedList.PrintList();
 
-	clsDblLinkedList<int>::Node* N1 = MyDblLinkedList.Find(2);
-
-    if (N1 != NULL)
-        cout << "\n" << "Node with value 2 is Found : -)" << "\n";
-    else
-        cout << "\n" << "Node with value 2 is Not Found : -(" << "\n";
-
-    MyDblLinkedList.InsertAfter(N1, 500);
-    cout << "\n" << "After Inserting 500 after 2:" << "\n";
-    MyDblLinkedList.PrintList();
-
-    MyDblLinkedList.InserAtEnd(700);
-    cout << "\n" << "After Inserting 700 at End:" << "\n";
-    MyDblLinkedList.PrintList();
-
-    clsDblLinkedList<int>::Node* N2 = MyDblLinkedList.Find(4);
-    MyDblLinkedList.DeleteNode(N2);
-    cout << "\n" << "After Deleting 4:" << "\n";
-    MyDblLinkedList.PrintList();
-
-    MyDblLinkedList.DeleteFirstNode();
-    cout << "\n" << "After Deleting First Node:" << "\n";
-    MyDblLinkedList.PrintList();
-
-    MyDblLinkedList.DeleteLastNode();
-    cout << "\n" << "After Deleting Last Node:" << "\n";
-    MyDblLinkedList.PrintList();
+    cout << "\n" << "Number of items in the linked list = " << MyDblLinkedList.Size();
 
     system("pause>0");
 }
